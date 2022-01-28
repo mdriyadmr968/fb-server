2e0938425a0e7c71f1370dd6c60ccf5325128cb4
mdriyadmr968 <mdriyadmr968@gmail.com>
2022-01-28 22:31:05 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+const cors = require("cors");
+const ObjectId = require("mongodb").ObjectId;
+
+const app = express();
+const port = 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://testuser5:MB6XmIFc2je60Uli@cluster0.5rfsc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("facebook");
+    const usersCollection = database.collection("email-password");
+
+    // post api 
+    app.post("/users", async (req, res) => {
+      const newUser = req.body;
+      const result = await usersCollection.insertOne(newUser);
+      console.log("got new user", req.body);
+      console.log("added user", result);
+      res.json(result);
+    });
+    
+
+
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+  res.send("Running my CRUD Server");
+});
+
+app.listen(port, () => {
+  console.log("Running Server on port", port);
+});
+
